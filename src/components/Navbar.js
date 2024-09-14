97d87124152adedf288b3ed047b938d279a6a9db
mynk09 <mynk.sss@gmail.com>
2024-09-15 03:59:11 +0530

    added word count and readtime

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 
 export default function Navbar(props) {  
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <a className="navbar-brand" href="/">
         {props.title}
       </a>
@@ -45,7 +45,7 @@ export default function Navbar(props) {
             aria-label="Search"
           />
           <button
-            className="btn btn-outline-success my-2 my-sm-0"
+            className="btn btn-outline-danger my-2 my-sm-0"
             type="submit"
           >
             Search
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -13,9 +13,16 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText)
     }
+
+    const handleLowClick = ()=> {
+        console.log("Uppercase button was clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
     const[text,setText] = useState('Enter your text')
   return (
-    <div>
+    <>
+        <div className='container'>
         <h1>{props.heading}</h1>   
       <div className="mb-3">
         <label for="myBox" className="form-label">
@@ -29,7 +36,17 @@ export default function TextForm(props) {
           rows="5"
         ></textarea>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>To UpperCase</button>
+      <button className="btn btn-primary mx-2" onClick={handleUpClick}>To UpperCase</button>
+      <button className="btn btn-secondary mx-2" onClick={handleLowClick}>To LowerCase</button>
     </div>
+    <div className='container my-3'>
+        <h2>Text Summary</h2>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008*text.split(" ").length} min read time</p>
+        <h2>Preview</h2>
+        <h4>{text}</h4>
+    </div>
+    </>
+    
   ); 
 }
